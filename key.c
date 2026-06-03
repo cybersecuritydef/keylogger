9943d93bd5fb70e33f7222d02db7acbebbbba0ac
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2026-06-03 17:29:14 +0300

    Update key.c

--- key.c
+++ key.c
@@ -57,15 +57,13 @@ void save_file(const char *filename, const char *key){
 }
 
 const char* key_value(const int code){
-	bool is_shift = false;
 	bool is_caps = false;
 	bool is_upper = false;
 	if(code >= 0 && code <= 255 && code != VK_CAPITAL && keys[code].en_key != NULL){
-        is_shift = (GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0;
+        is_upper = (GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0;
 		is_caps = (GetKeyState(VK_CAPITAL) & 0x0001) != 0;
-        is_upper = is_shift;
         if (code >= VK_A && code <= VK_Z)            
-            is_upper = is_shift ^ is_caps;
+            is_upper = is_upper ^ is_caps;
         if(is_key_ru()){
             if(is_upper)
                 return keys[code].ru_key_shift;
